297c48c4640a64ff8f629eba7293badeec360b4f
luanaschuler <66922010+luanashuler@users.noreply.github.com>
2020-10-26 15:06:33 -0300

    Classes Entity, Contract, DBHelper, DBGateway

---
+++ app/src/main/java/br/senai/sc/projeto01/database/contract/ProdutoContract.java
@@ -0,0 +1,18 @@
+package br.senai.sc.projeto01.database.contract;
+
+import br.senai.sc.projeto01.database.entity.ProdutoEntity;
+
+public final class ProdutoContract {
+
+    private ProdutoContract() {}
+
+    public static final String criarTabela() {
+        return "CREATE TABLE " + ProdutoEntity.TABLE_NAME + " (" +
+                ProdutoEntity._ID + "INTEGER PRIMARY KEY," +
+                ProdutoEntity.COLUMN_NAME_NOME + " TEXT," +
+                ProdutoEntity.COLUMN_NAME_VALOR + " Real)";
+    }
+    public static final String removerTabela() {
+        return "DROP TABLE IF EXISTS " + ProdutoEntity.TABLE_NAME;
+    }
+}
